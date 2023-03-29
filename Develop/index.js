bc5083a1568af712468dde1486fc8938ff520358
Matthew Standish <mwstandish@aol.com>
2023-03-28 20:23:00 -0400

    added packages and questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a brief description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide installation instructions for your project:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide usage instructions for your project:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use for your project?",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3-Clause", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please provide guidelines for contributing to your project:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please provide instructions for running tests for your project:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
